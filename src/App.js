e5409f7fad1ae1fbb0fa71cdba3f13471ca45234
coderfirst <50993530+ssjha0542@users.noreply.github.com>
2022-01-25 02:16:55 +0530

    implemented the design of page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./styles.css";
-
+import axios from "axios";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div class="container">
+        <h1>Search your Books</h1>
+        <form>
+          <div class="form-group">
+            <input
+              type="text"
+              className="form-control mt-8"
+              placeholder="Search books here"
+              autoComplete="off"
+            />
+          </div>
+          <button type="submit" className="btn btn-info">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from 'axios';
+import App from "./App";
 
-import "./styles.css"
-
-function App(){
-  return(
-    <div class="container">
-      <form>
-        <div class="form-group">
-          <input type="text" className="input-control" placeholder="Search for books"autoComplete="off"/>
-        </div>
-        <button type="submit"className="btn btn-danger">Search</button>
-      </form>
-    </div>
-  )
-}
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-    <App />,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
